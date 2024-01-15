3515fb1ce17431cd19bd2e97aa1eb76e7b563cda
does <ldk980130@gmail.com>
2024-01-15 14:31:20 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/birca/bircabackend/BircaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.birca.bircabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BircaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BircaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/birca/bircabackend/BircaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birca.bircabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BircaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
