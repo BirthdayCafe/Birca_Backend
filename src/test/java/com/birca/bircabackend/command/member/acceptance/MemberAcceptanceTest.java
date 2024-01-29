06f43945f4036927c58c0b53aeef183450bc03f9
does <ldk980130@gmail.com>
2024-01-29 20:47:34 +0900

    test: 닉네임 등록 인수 테스트 및 문서화 테스트 추가

--- src/test/java/com/birca/bircabackend/command/member/acceptance/MemberAcceptanceTest.java
+++ src/test/java/com/birca/bircabackend/command/member/acceptance/MemberAcceptanceTest.java
@@ -1,5 +1,6 @@
 package com.birca.bircabackend.command.member.acceptance;
 
+import com.birca.bircabackend.command.member.dto.NicknameRegisterRequest;
 import com.birca.bircabackend.command.member.dto.RoleChangeRequest;
 import com.birca.bircabackend.support.enviroment.AcceptanceTest;
 import io.restassured.RestAssured;
@@ -16,23 +17,43 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Sql("/fixture/member-fixture.sql")
 class MemberAcceptanceTest extends AcceptanceTest {
 
+    private static final Long MEMBER_ID = 1L;
+
     @Test
     void 회원의_역할을_변경한다() {
         // given
-        Long memberId = 1L;
         RoleChangeRequest request = new RoleChangeRequest("HOST");
 
         // when
         ExtractableResponse<Response> response = RestAssured.given().log().all()
                 .when()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .header(HttpHeaders.AUTHORIZATION, bearerTokenProvider.getToken(memberId))
+                .header(HttpHeaders.AUTHORIZATION, bearerTokenProvider.getToken(MEMBER_ID))
                 .body(request)
                 .post("/api/v1/members/role-change")
                 .then().log().all()
                 .extract();
 
         //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @Test
+    void 회원의_닉네임을_등록한다() {
+        // given
+        NicknameRegisterRequest request = new NicknameRegisterRequest("등록 닉네임");
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, bearerTokenProvider.getToken(MEMBER_ID))
+                .body(request)
+                .post("/api/v1/join/register-nickname")
+                .then().log().all()
+                .extract();
+
+        // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 }
--- src/test/java/com/birca/bircabackend/command/member/presentation/MemberControllerTest.java
+++ src/test/java/com/birca/bircabackend/command/member/presentation/MemberControllerTest.java
@@ -1,33 +1,58 @@
 package com.birca.bircabackend.command.member.presentation;
 
+import com.birca.bircabackend.command.member.dto.NicknameRegisterRequest;
 import com.birca.bircabackend.command.member.dto.RoleChangeRequest;
 import com.birca.bircabackend.support.enviroment.DocumentationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class MemberControllerTest extends DocumentationTest {
 
+    private static final Long MEMBER_ID = 1L;
+
     @Test
     void 회원의_역할을_변경한다() throws Exception {
         // given
-        Long memberId = 1L;
         RoleChangeRequest request = new RoleChangeRequest("HOST");
 
         // when
         ResultActions result = mockMvc.perform(post("/api/v1/members/role-change")
                 .contentType(MediaType.APPLICATION_JSON)
-                .header(HttpHeaders.AUTHORIZATION, bearerTokenProvider.getToken(memberId))
+                .header(HttpHeaders.AUTHORIZATION, bearerTokenProvider.getToken(MEMBER_ID))
                 .content(objectMapper.writeValueAsString(request))
         );
 
         // then
         result.andExpect((status().isOk()))
                 .andDo(document("member-role-change", HOST_INFO));
+    }
+
+    @Test
+    void 회원의_닉네임을_등록한다() throws Exception {
+        // given
+        NicknameRegisterRequest request = new NicknameRegisterRequest("닉네임");
+
+        // when
+        ResultActions result = mockMvc.perform(post("/api/v1/join/register-nickname")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, bearerTokenProvider.getToken(MEMBER_ID))
+                .content(objectMapper.writeValueAsString(request))
+        );
+
+        // then
+        result.andExpect((status().isOk()))
+                .andDo(document("member-nickname-register", HOST_INFO,
+                        requestFields(
+                                fieldWithPath("nickname").type(JsonFieldType.BOOLEAN).description("등록할 닉네임")
+                        )
+                ));
     }
 }
