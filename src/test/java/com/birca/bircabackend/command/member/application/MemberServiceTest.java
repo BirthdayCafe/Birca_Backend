8f3fad2ae4345c4431bd28443175827b5e370bba
does <ldk980130@gmail.com>
2024-01-29 17:48:49 +0900

    test: 없는 회원인 경우 검증

--- src/test/java/com/birca/bircabackend/command/member/application/MemberServiceTest.java
+++ src/test/java/com/birca/bircabackend/command/member/application/MemberServiceTest.java
@@ -3,15 +3,19 @@ package com.birca.bircabackend.command.member.application;
 import com.birca.bircabackend.command.auth.login.LoginMember;
 import com.birca.bircabackend.command.member.MemberFixtureRepository;
 import com.birca.bircabackend.command.member.dto.RoleChangeRequest;
+import com.birca.bircabackend.command.member.exception.MemberErrorCode;
+import com.birca.bircabackend.common.exception.BusinessException;
 import com.birca.bircabackend.support.enviroment.ServiceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Sql("/fixture/member-fixture.sql")
 class MemberServiceTest extends ServiceTest {
@@ -44,6 +48,18 @@ class MemberServiceTest extends ServiceTest {
                     .map(member -> member.getRole().name())
                     .get()
                     .isEqualTo(role);
+        }
+
+        @Test
+        void 변경할_때_없는_회원인_경우를_검증한다() {
+            // given
+            RoleChangeRequest request = new RoleChangeRequest("HOST");
+
+            // when // then
+            assertThatThrownBy(() -> memberService.changeMemberRole(request, new LoginMember(100L)))
+                    .isInstanceOf(BusinessException.class)
+                    .extracting("errorCode")
+                    .isEqualTo(MemberErrorCode.MEMBER_NOT_FOUND);
         }
     }
 }
