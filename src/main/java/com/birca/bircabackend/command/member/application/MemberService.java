26de5d93ca392c9aea71189b1a32986039ef6e5e
does <ldk980130@gmail.com>
2024-01-29 21:46:42 +0900

    refactor: findById를 모든 엔티티에서 사용 가능하도록 리팩터링

--- src/main/java/com/birca/bircabackend/command/member/application/MemberService.java
+++ src/main/java/com/birca/bircabackend/command/member/application/MemberService.java
@@ -7,11 +7,14 @@ import com.birca.bircabackend.command.member.domain.MemberRole;
 import com.birca.bircabackend.command.member.domain.Nickname;
 import com.birca.bircabackend.command.member.dto.NicknameRegisterRequest;
 import com.birca.bircabackend.command.member.dto.RoleChangeRequest;
-import com.birca.bircabackend.command.member.exception.MemberErrorCode;
+import com.birca.bircabackend.common.EntityUtil;
 import com.birca.bircabackend.common.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.birca.bircabackend.command.member.exception.MemberErrorCode.DUPLICATED_NICKNAME;
+import static com.birca.bircabackend.command.member.exception.MemberErrorCode.MEMBER_NOT_FOUND;
 
 @Component
 @Transactional
@@ -19,9 +22,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final EntityUtil entityUtil;
 
     public void changeMemberRole(RoleChangeRequest request, LoginMember loginMember) {
-        Member member = getMember(loginMember.id());
+        Member member = entityUtil.getEntity(Member.class, loginMember.id(), MEMBER_NOT_FOUND);
         MemberRole role = MemberRole.from(request.role());
         member.changeRole(role);
     }
@@ -29,18 +33,13 @@ public class MemberService {
     public void registerNickname(NicknameRegisterRequest request, LoginMember loginMember) {
         Nickname nickname = new Nickname(request.nickname());
         validateDuplicatedNickname(nickname);
-        Member member = getMember(loginMember.id());
+        Member member = entityUtil.getEntity(Member.class, loginMember.id(), MEMBER_NOT_FOUND);
         member.registerNickname(nickname);
     }
 
     private void validateDuplicatedNickname(Nickname nickname) {
         if (memberRepository.existsByNickname(nickname)) {
-            throw BusinessException.from(MemberErrorCode.DUPLICATED_NICKNAME);
+            throw BusinessException.from(DUPLICATED_NICKNAME);
         }
-    }
-
-    private Member getMember(Long memberId) {
-        return memberRepository.findById(memberId)
-                .orElseThrow(() -> BusinessException.from(MemberErrorCode.MEMBER_NOT_FOUND));
     }
 }
--- src/main/java/com/birca/bircabackend/command/member/domain/MemberRepository.java
+++ src/main/java/com/birca/bircabackend/command/member/domain/MemberRepository.java
@@ -2,11 +2,7 @@ package com.birca.bircabackend.command.member.domain;
 
 import org.springframework.data.repository.Repository;
 
-import java.util.Optional;
-
 public interface MemberRepository extends Repository<Member, Long> {
-
-    Optional<Member> findById(Long id);
 
     boolean existsByNickname(Nickname nickname);
 }
