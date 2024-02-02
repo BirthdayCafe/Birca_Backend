99807e647d529406b21b7036977487a2727ba99c
does <ldk980130@gmail.com>
2024-02-03 00:29:52 +0900

    refactor: 관심 아티스트 등록 검증 코드를 다른 클래스로 분리

--- src/main/java/com/birca/bircabackend/command/artist/application/ArtistService.java
+++ src/main/java/com/birca/bircabackend/command/artist/application/ArtistService.java
@@ -9,10 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashSet;
 import java.util.List;
 
-import static com.birca.bircabackend.command.artist.exception.ArtistErrorCode.*;
 import static com.birca.bircabackend.command.artist.exception.ArtistErrorCode.NOT_EXIST_ARTIST;
 
 @Service
@@ -22,8 +20,8 @@ public class ArtistService {
 
     private final ArtistRepository artistRepository;
     private final FavoriteArtistRepository favoriteArtistRepository;
-    private final InterestArtistRepository interestArtistRepository;
     private final InterestArtistBulkSaveRepository interestArtistBulkSaveRepository;
+    private final InterestArtistRegisterValidator interestArtistRegisterValidator;
 
     public void registerFavoriteArtist(FavoriteArtistRequest request,
                                        LoginMember loginMember) {
@@ -45,58 +43,10 @@ public class ArtistService {
 
     public void registerInterestArtist(List<InterestArtistRequest> request, LoginMember loginMember) {
         Long fanId = loginMember.id();
-        List<Long> requestArtistIds = extractArtistIds(request);
-        List<Long> registeredInterestArtistIds = getRegisteredArtistIds(fanId);
-
-        validateArtistExceed(requestArtistIds.size(), registeredInterestArtistIds.size());
-        validateDuplicateArtists(registeredInterestArtistIds, requestArtistIds);
-        validateExistAllArtist(requestArtistIds);
-
-        interestArtistBulkSaveRepository.saveAll(
-                requestArtistIds.stream()
-                        .map(artistId -> new InterestArtist(fanId, artistId))
-                        .toList()
-        );
-    }
-
-    private List<Long> getRegisteredArtistIds(Long fanId) {
-        return interestArtistRepository.findByFanId(fanId)
-                .stream()
-                .map(InterestArtist::getArtistId)
+        List<InterestArtist> interestArtists = request.stream()
+                .map(req -> new InterestArtist(fanId, req.artistId()))
                 .toList();
-    }
-
-    private List<Long> extractArtistIds(List<InterestArtistRequest> request) {
-        return request.stream()
-                .map(InterestArtistRequest::artistId)
-                .toList();
-    }
-
-    private void validateArtistExceed(int requestSize, int registeredSize) {
-        if (requestSize + registeredSize > InterestArtist.REGISTER_LIMIT) {
-            throw BusinessException.from(EXCEED_INTEREST_LIMIT);
-        }
-    }
-
-    private void validateDuplicateArtists(List<Long> registeredInterestArtistIds,
-                                          List<Long> requestArtistIds) {
-        if (requestArtistIds.size() != new HashSet<>(requestArtistIds).size()) {
-            throw BusinessException.from(DUPLICATE_INTEREST_ARTIST);
-        }
-        for (Long registeredInterestArtistId : registeredInterestArtistIds) {
-            validateRegisteredIdInRequest(requestArtistIds, registeredInterestArtistId);
-        }
-    }
-
-    private void validateRegisteredIdInRequest(List<Long> requestArtistIds, Long registeredInterestArtistId) {
-        if (requestArtistIds.contains(registeredInterestArtistId)) {
-            throw BusinessException.from(DUPLICATE_INTEREST_ARTIST);
-        }
-    }
-
-    private void validateExistAllArtist(List<Long> artistIds) {
-        if (artistRepository.countByIdIn(artistIds) != artistIds.size()) {
-            throw BusinessException.from(NOT_EXIST_ARTIST);
-        }
+        interestArtistRegisterValidator.validate(fanId, interestArtists);
+        interestArtistBulkSaveRepository.saveAll(interestArtists);
     }
 }
