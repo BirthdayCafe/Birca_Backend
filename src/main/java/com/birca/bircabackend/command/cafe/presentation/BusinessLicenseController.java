c61bbb156d34a4eddd5725674420add61a98c569
CMHyuk <alsgur9710@naver.com>
2024-02-08 14:31:36 +0900

    refactor: BusinessLicenseFacade로 리팩토링

--- src/main/java/com/birca/bircabackend/command/cafe/presentation/BusinessLicenseController.java
+++ src/main/java/com/birca/bircabackend/command/cafe/presentation/BusinessLicenseController.java
@@ -2,10 +2,12 @@ package com.birca.bircabackend.command.cafe.presentation;
 
 import com.birca.bircabackend.command.auth.authorization.LoginMember;
 import com.birca.bircabackend.command.auth.authorization.RequiredLogin;
+import com.birca.bircabackend.command.cafe.application.BusinessLicenseFacade;
 import com.birca.bircabackend.command.cafe.application.BusinessLicenseProcessingService;
 import com.birca.bircabackend.command.cafe.application.BusinessLicenseService;
 import com.birca.bircabackend.command.cafe.dto.BusinessLicenseCreateRequest;
 import com.birca.bircabackend.command.cafe.dto.BusinessLicenseResponse;
+import com.birca.bircabackend.command.cafe.validation.UploadCountCheck;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,13 +21,17 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class BusinessLicenseController {
 
+    private final BusinessLicenseFacade businessLicenseFacade;
     private final BusinessLicenseService businessLicenseService;
     private final BusinessLicenseProcessingService businessLicenseProcessingService;
 
     @PostMapping("/v1/cafes/license-read")
     @RequiredLogin
-    public ResponseEntity<BusinessLicenseResponse> readBusinessLicense(@ModelAttribute MultipartFile businessLicense) {
-        BusinessLicenseResponse response = businessLicenseProcessingService.getBusinessLicenseInfo(businessLicense);
+    @UploadCountCheck
+    public ResponseEntity<BusinessLicenseResponse> readBusinessLicense(LoginMember loginMember,
+                                                                           @ModelAttribute MultipartFile businessLicense) {
+        BusinessLicenseResponse response
+                = businessLicenseFacade.getBusinessLicenseInfoAndUploadCount(loginMember, businessLicense);
         return ResponseEntity.ok(response);
     }
 
