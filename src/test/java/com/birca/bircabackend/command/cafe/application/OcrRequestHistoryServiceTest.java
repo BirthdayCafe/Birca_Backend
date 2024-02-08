458cbba12571516776351ebb1e2bf3fa0477d56b
CMHyuk <alsgur9710@naver.com>
2024-02-08 12:24:25 +0900

    test: 사업자등록증 업로드 횟수 카운트 테스트

---
+++ src/test/java/com/birca/bircabackend/command/cafe/application/OcrRequestHistoryServiceTest.java
@@ -0,0 +1,33 @@
+package com.birca.bircabackend.command.cafe.application;
+
+import com.birca.bircabackend.command.cafe.domain.OcrRequestHistory;
+import com.birca.bircabackend.command.cafe.domain.OcrRequestHistoryRepository;
+import com.birca.bircabackend.command.cafe.dto.UploadCountResponse;
+import com.birca.bircabackend.support.enviroment.ServiceTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OcrRequestHistoryServiceTest extends ServiceTest {
+
+    @Autowired
+    private OcrRequestHistoryService ocrRequestHistoryService;
+
+    @Autowired
+    private OcrRequestHistoryRepository ocrRequestHistoryRepository;
+
+    @Test
+    void 사업자등록증_업로드_횟수를_증가시킨다() {
+        // given
+        Long ownerId = 1L;
+        OcrRequestHistory ocrRequestHistory = new OcrRequestHistory(ownerId, 1);
+        ocrRequestHistoryRepository.save(ocrRequestHistory);
+
+        // when
+        UploadCountResponse uploadCountResponse = ocrRequestHistoryService.incrementUploadCount(ownerId);
+
+        // then
+        assertThat(uploadCountResponse.uploadCount()).isEqualTo(2);
+    }
+}
