5ff132d3ea26332258eb0301793e72d8de0ce59c
does <ldk980130@gmail.com>
2024-02-11 17:27:26 +0900

    chore: 예상치 못한 예외일 겨우 로그 남기기

--- src/main/java/com/birca/bircabackend/common/exception/presentation/GlobalExceptionController.java
+++ src/main/java/com/birca/bircabackend/common/exception/presentation/GlobalExceptionController.java
@@ -4,16 +4,23 @@ import com.birca.bircabackend.common.exception.BusinessException;
 import com.birca.bircabackend.common.exception.ErrorCode;
 import com.birca.bircabackend.common.exception.ExceptionResponse;
 import com.birca.bircabackend.common.exception.InternalServerErrorCode;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionController {
 
     @ExceptionHandler
     public ResponseEntity<ExceptionResponse> handleBusinessException(BusinessException exception) {
         ErrorCode errorCode = exception.getErrorCode();
+        if (errorCode.getHttpStatusCode() == INTERNAL_SERVER_ERROR.value()) {
+            log.error(errorCode.getMessage());
+        }
         return ResponseEntity.status(errorCode.getHttpStatusCode())
                 .body(ExceptionResponse.from(errorCode));
     }
@@ -21,6 +28,7 @@ public class GlobalExceptionController {
     @ExceptionHandler
     public ResponseEntity<ExceptionResponse> handleUnExpectedException(Exception exception) {
         InternalServerErrorCode errorCode = new InternalServerErrorCode(exception.getMessage());
+        log.error(errorCode.getMessage());
         return ResponseEntity.status(errorCode.getHttpStatusCode())
                 .body(ExceptionResponse.from(errorCode));
     }
