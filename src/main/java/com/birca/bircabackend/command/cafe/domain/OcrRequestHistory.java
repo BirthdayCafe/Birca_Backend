aad0110ed21770f917f485b1d258366acdfe78d5
CMHyuk <alsgur9710@naver.com>
2024-02-14 00:28:20 +0900

    refactor: 상수 위치 맨 위로 변경

--- src/main/java/com/birca/bircabackend/command/cafe/domain/OcrRequestHistory.java
+++ src/main/java/com/birca/bircabackend/command/cafe/domain/OcrRequestHistory.java
@@ -1,27 +1,24 @@
 package com.birca.bircabackend.command.cafe.domain;
 
 import com.birca.bircabackend.common.domain.BaseEntity;
-import com.birca.bircabackend.common.exception.BusinessException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import static com.birca.bircabackend.command.cafe.exception.BusinessLicenseErrorCode.INVALID_UPLOAD_COUNT;
-
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class OcrRequestHistory extends BaseEntity {
+
+    private static final int DEFAULT_UPLOAD_COUNT = 0;
 
     @Column(nullable = false)
     private Long ownerId;
 
     @Column(nullable = false)
     private Integer uploadCount;
-
-    private static final int DEFAULT_UPLOAD_COUNT = 0;
 
     public Integer incrementUploadCount() {
         return ++uploadCount;
