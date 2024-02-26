e3b97a39b9697e27c973dd009a97ceea4fd3f55b
does <ldk980130@gmail.com>
2024-02-26 21:49:45 +0900

    feat: 연락처 형식 검증 기능 구현

--- src/main/java/com/birca/bircabackend/command/birca/domain/value/PhoneNumber.java
+++ src/main/java/com/birca/bircabackend/command/birca/domain/value/PhoneNumber.java
@@ -1,5 +1,7 @@
 package com.birca.bircabackend.command.birca.domain.value;
 
+import com.birca.bircabackend.command.birca.exception.BirthdayCafeErrorCode;
+import com.birca.bircabackend.common.exception.BusinessException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
@@ -7,11 +9,15 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.regex.Pattern;
+
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @EqualsAndHashCode
 public class PhoneNumber {
+
+    private static final Pattern VALID_PATTERN = Pattern.compile("^\\d{3}-\\d{3,4}-\\d{4}$");
 
     @Column(name = "host_phone_number", nullable = false)
     private String value;
@@ -21,6 +27,13 @@ public class PhoneNumber {
     }
 
     private PhoneNumber(String value) {
+        validateInvalidPattern(value);
         this.value = value;
+    }
+
+    private void validateInvalidPattern(String value) {
+        if (!VALID_PATTERN.matcher(value).matches()) {
+            throw BusinessException.from(BirthdayCafeErrorCode.INVALID_PHONE_NUMBER);
+        }
     }
 }
--- src/main/java/com/birca/bircabackend/command/birca/exception/BirthdayCafeErrorCode.java
+++ src/main/java/com/birca/bircabackend/command/birca/exception/BirthdayCafeErrorCode.java
@@ -11,7 +11,8 @@ public enum BirthdayCafeErrorCode implements ErrorCode {
 
     INVALID_SCHEDULE(5001, 400, "생일카페 시작일은 종료일 보다 앞일 수 없습니다."),
     INVALID_VISITANTS(5002, 400, "최소 방문자는 최대 방문자 보다 클 수 없고 자연수여야 합니다."),
-    RENTAL_PENDING_EXISTS(5003, 400, "대관 대기 상태인 생일 카페가 이미 존재합니다.")
+    RENTAL_PENDING_EXISTS(5003, 400, "대관 대기 상태인 생일 카페가 이미 존재합니다."),
+    INVALID_PHONE_NUMBER(5004, 400, "올바르지 않은 연락처 형식입니다.")
 
     ;
 
