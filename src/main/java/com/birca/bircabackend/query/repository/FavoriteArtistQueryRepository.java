7f9c52c3d75cb8a445216f060f94480fa6510a62
CMHyuk <alsgur9710@naver.com>
2024-02-26 10:45:43 +0900

    refactor: 서브쿼리로 한 번에 가져오도록 수정

--- src/main/java/com/birca/bircabackend/query/repository/FavoriteArtistQueryRepository.java
+++ src/main/java/com/birca/bircabackend/query/repository/FavoriteArtistQueryRepository.java
@@ -1,11 +1,14 @@
 package com.birca.bircabackend.query.repository;
 
+import com.birca.bircabackend.command.artist.domain.Artist;
 import com.birca.bircabackend.command.artist.domain.FavoriteArtist;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
+import java.util.Optional;
+
 public interface FavoriteArtistQueryRepository extends Repository<FavoriteArtist, Long> {
 
-    @Query("SELECT f.artistId FROM FavoriteArtist f WHERE f.fanId = :fanId")
-    Long findArtistIdByFanId(Long fanId);
+    @Query("SELECT a FROM Artist a WHERE a.id = (SELECT i.artistId FROM FavoriteArtist i WHERE i.fanId = :fanId)")
+    Optional<Artist> findArtistIdByFanId(Long fanId);
 }
