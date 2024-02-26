d96be1f524c76aae61e21e9ebc17834091028f4b
CMHyuk <alsgur9710@naver.com>
2024-02-26 15:24:32 +0900

    fix: 회원 존재 유무 검증 후 아티스트 조회하도록 수정

--- src/main/java/com/birca/bircabackend/query/service/InterestArtistQueryService.java
+++ src/main/java/com/birca/bircabackend/query/service/InterestArtistQueryService.java
@@ -1,6 +1,8 @@
 package com.birca.bircabackend.query.service;
 
 import com.birca.bircabackend.command.auth.authorization.LoginMember;
+import com.birca.bircabackend.command.member.domain.Member;
+import com.birca.bircabackend.common.EntityUtil;
 import com.birca.bircabackend.query.dto.ArtistResponse;
 import com.birca.bircabackend.query.repository.InterestArtistQueryRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +11,19 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+import static com.birca.bircabackend.command.member.exception.MemberErrorCode.MEMBER_NOT_FOUND;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class InterestArtistQueryService {
 
     private final InterestArtistQueryRepository interestArtistQueryRepository;
+    private final EntityUtil entityUtil;
 
     public List<ArtistResponse> findInterestArtists(LoginMember loginMember) {
-        return interestArtistQueryRepository.findInterestArtistsByFanId(loginMember.id())
+        Member member = entityUtil.getEntity(Member.class, loginMember.id(), MEMBER_NOT_FOUND);
+        return interestArtistQueryRepository.findInterestArtistsByFanId(member.getId())
                 .stream()
                 .map(ArtistResponse::new)
                 .toList();
