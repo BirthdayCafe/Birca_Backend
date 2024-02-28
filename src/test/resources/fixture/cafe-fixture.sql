f4c393e9f50aad24b81594ccba5a34beb8ab4dd7
CMHyuk <alsgur9710@naver.com>
2024-02-28 13:35:14 +0900

    fix: 충돌 해결

--- src/test/resources/fixture/cafe-fixture.sql
+++ src/test/resources/fixture/cafe-fixture.sql
@@ -1,8 +1,8 @@
 SET foreign_key_checks = 0;
 
-INSERT INTO cafe (id, business_license_id, name)
-VALUES (1, 1, '미스티우드'),
-       (2, 2, '우지커피'),
-       (3, 3, '메가커피');
+INSERT INTO cafe (id, owner_id, business_license_id, name)
+VALUES (1, 1, 1, '미스티우드'),
+       (2, 2, 2, '우지커피'),
+       (3, 3, 3, '메가커피');
 
 SET foreign_key_checks = 1;
