f1b31d5348c557dc25d2082b994a7fc171a42c34
CMHyuk <alsgur9710@naver.com>
2024-03-08 15:40:34 +0900

    feat: ArtistGroup의 name 컬럼 인덱스 적용 명시적으로 작성

--- src/main/java/com/birca/bircabackend/command/artist/domain/ArtistGroup.java
+++ src/main/java/com/birca/bircabackend/command/artist/domain/ArtistGroup.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(indexes = @Index(name = "IDX_NAME", columnList = "name"))
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class ArtistGroup extends BaseEntity {
