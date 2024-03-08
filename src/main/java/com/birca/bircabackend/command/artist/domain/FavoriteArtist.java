3f883a0f9af3e7fe4b5a7f3456f28b9ac303291b
CMHyuk <alsgur9710@naver.com>
2024-03-08 15:40:06 +0900

    feat: 유니크 제약 조건 명시적으로 작성

--- src/main/java/com/birca/bircabackend/command/artist/domain/FavoriteArtist.java
+++ src/main/java/com/birca/bircabackend/command/artist/domain/FavoriteArtist.java
@@ -3,11 +3,16 @@ package com.birca.bircabackend.command.artist.domain;
 import com.birca.bircabackend.common.domain.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(uniqueConstraints = {@UniqueConstraint(
+        name = "uc_favoriteartist_fan", columnNames = {"fanId"}
+)})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class FavoriteArtist extends BaseEntity {
--- src/main/java/com/birca/bircabackend/command/cafe/domain/Cafe.java
+++ src/main/java/com/birca/bircabackend/command/cafe/domain/Cafe.java
@@ -1,26 +1,26 @@
 package com.birca.bircabackend.command.cafe.domain;
 
 import com.birca.bircabackend.common.domain.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Index;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(uniqueConstraints = {@UniqueConstraint(
+        name = "uc_cafe_businesslicenseid",
+        columnNames = {"businessLicenseId"}
+)})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(indexes = @Index(name = "IDX_CAFE_NAME", columnList = "name"))
 @Getter
 public class Cafe extends BaseEntity {
 
     @Column(nullable = false, unique = true)
     private Long businessLicenseId;
-  
+
     @Column(nullable = false)
     private Long ownerId;
-  
+
     @Column(nullable = false)
     private String name;
 }
