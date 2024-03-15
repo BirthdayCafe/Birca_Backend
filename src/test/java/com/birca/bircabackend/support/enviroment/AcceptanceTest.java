384e9d9e6d1ed8e981a287910ff8973879d07ed9
CMHyuk <alsgur9710@naver.com>
2024-03-16 01:40:08 +0900

    test: ImageUploader 모킹

--- src/test/java/com/birca/bircabackend/support/enviroment/AcceptanceTest.java
+++ src/test/java/com/birca/bircabackend/support/enviroment/AcceptanceTest.java
@@ -1,28 +1,16 @@
 package com.birca.bircabackend.support.enviroment;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectResult;
 import com.birca.bircabackend.support.TestBearerTokenProvider;
 import com.birca.bircabackend.support.isolation.DatabaseIsolation;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DatabaseIsolation
-public class AcceptanceTest extends MockOAuthEnvironment {
-
-    private static final String URL = "https://s3.ap-northeast-2.amazonaws.com/fileupload/image/example.png";
+public class AcceptanceTest extends MockEnvironment {
 
     @LocalServerPort
     private int port;
@@ -34,15 +22,4 @@ public class AcceptanceTest extends MockOAuthEnvironment {
 
     @Autowired
     protected TestBearerTokenProvider bearerTokenProvider;
-
-    @MockBean
-    private AmazonS3 amazonS3;
-
-    @BeforeEach
-    void mockAwsS3() throws MalformedURLException {
-        given(amazonS3.putObject(any(PutObjectRequest.class)))
-                .willReturn(new PutObjectResult());
-        given(amazonS3.getUrl(any(), any()))
-                .willReturn(new URL(URL));
-    }
 }
--- src/test/java/com/birca/bircabackend/support/enviroment/MockEnvironment.java
+++ src/test/java/com/birca/bircabackend/support/enviroment/MockEnvironment.java
@@ -3,14 +3,16 @@ package com.birca.bircabackend.support.enviroment;
 import com.birca.bircabackend.command.auth.application.oauth.OAuthMember;
 import com.birca.bircabackend.command.auth.application.oauth.OAuthProvider;
 import com.birca.bircabackend.command.auth.application.oauth.OAuthProviderFactory;
+import com.birca.bircabackend.common.upload.ImageUploader;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
-public class MockOAuthEnvironment {
+public class MockEnvironment {
 
+    private static final String URL = "https://s3.ap-northeast-2.amazonaws.com/fileupload/image/example.png";
     private static final OAuthProvider MOCK_PROVIDER = new OAuthProvider() {
         @Override
         public OAuthMember getOAuthMember(String accessToken) {
@@ -26,9 +28,18 @@ public class MockOAuthEnvironment {
     @MockBean
     private OAuthProviderFactory providerFactory;
 
+    @MockBean
+    private ImageUploader imageUploader;
+
     @BeforeEach
     void mockOAuthProvider() {
         given(providerFactory.getProvider(any()))
                 .willReturn(MOCK_PROVIDER);
+    }
+
+    @BeforeEach
+    void mockImageUploader() {
+        given(imageUploader.upload(any()))
+                .willReturn(URL);
     }
 }
