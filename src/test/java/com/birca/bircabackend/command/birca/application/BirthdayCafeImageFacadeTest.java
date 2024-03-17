cc51009689cb22f539680e570900fbb5393aef88
CMHyuk <alsgur9710@naver.com>
2024-03-17 10:48:37 +0900

    test: BirthdayCafeImageFacade 예외 테스트 추가

--- src/test/java/com/birca/bircabackend/command/birca/application/BirthdayCafeImageFacadeTest.java
+++ src/test/java/com/birca/bircabackend/command/birca/application/BirthdayCafeImageFacadeTest.java
@@ -1,11 +1,8 @@
 package com.birca.bircabackend.command.birca.application;
 
-import com.birca.bircabackend.command.birca.domain.BirthdayCafeImage;
 import com.birca.bircabackend.command.birca.exception.BirthdayCafeErrorCode;
 import com.birca.bircabackend.common.exception.BusinessException;
 import com.birca.bircabackend.support.enviroment.ServiceTest;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -14,22 +11,16 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-@Sql("/fixture/birthday-cafe-fixture.sql")
+@Sql("/fixture/birthday-cafe-image-fixture.sql")
 class BirthdayCafeImageFacadeTest extends ServiceTest {
 
     @Autowired
-    protected BirthdayCafeImageFacade birthdayCafeImageFacade;
-
-    @PersistenceContext
-    private EntityManager entityManager;
+    private BirthdayCafeImageFacade birthdayCafeImageFacade;
 
     private static final MultipartFile BIRTHDAY_CAFE_IMAGE =
             new MockMultipartFile("birthdayCafeImage",  "image1".getBytes());
@@ -41,16 +32,25 @@ class BirthdayCafeImageFacadeTest extends ServiceTest {
         @Test
         void 정상적으로_저장한다() {
             // given
-            Long birthdayCafeId = 1L;
+            Long birthdayCafeId = 2L;
 
             // when
             birthdayCafeImageFacade.save(birthdayCafeId, BIRTHDAY_CAFE_IMAGE);
-            List<BirthdayCafeImage> images = entityManager.createQuery("select bci from BirthdayCafeImage bci", BirthdayCafeImage.class)
-                    .getResultList();
 
             // then
-            assertThat(images.size()).isEqualTo(1);
             verify(imageUploader, times(1)).upload(any());
+        }
+
+        @Test
+        void 기본_이미지수가_10이_초과되면_예외가_발생한다() {
+            // given
+            Long birthdayCafeId = 1L;
+
+            // when then
+            assertThatThrownBy(() -> birthdayCafeImageFacade.save(birthdayCafeId, BIRTHDAY_CAFE_IMAGE))
+                    .isInstanceOf(BusinessException.class)
+                    .extracting("errorCode")
+                    .isEqualTo(BirthdayCafeErrorCode.INVALID_UPLOAD_SIZE_REQUEST);
         }
 
         @Test
