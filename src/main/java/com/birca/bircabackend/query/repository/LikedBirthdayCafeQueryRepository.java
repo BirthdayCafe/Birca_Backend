8cd1c189702525ed2ff1f252eac67792850c4cb8
does <ldk980130@gmail.com>
2024-03-22 13:48:44 +0900

    style: 쿼리에 공백 추가

--- src/main/java/com/birca/bircabackend/query/repository/LikedBirthdayCafeQueryRepository.java
+++ src/main/java/com/birca/bircabackend/query/repository/LikedBirthdayCafeQueryRepository.java
@@ -12,7 +12,7 @@ public interface LikedBirthdayCafeQueryRepository extends Repository<Like, Long>
 
     @Query("select new com.birca.bircabackend.query.repository.model.BirthdayCafeView(bc, bci, a, ag) " +
             "from Like lk " +
-            "join BirthdayCafe bc on bc.id = lk.target.targetId and lk.target.targetType = 'BIRTHDAY_CAFE'" +
+            "join BirthdayCafe bc on bc.id = lk.target.targetId and lk.target.targetType = 'BIRTHDAY_CAFE' " +
             "left join BirthdayCafeImage bci on bci.birthdayCafeId = bc.id and bci.isMain = true " +
             "left join Artist a on a.id = bc.artistId " +
             "left join ArtistGroup ag on a.groupId = ag.id " +
