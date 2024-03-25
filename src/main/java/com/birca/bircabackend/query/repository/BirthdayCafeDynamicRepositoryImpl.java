b30ad6fda81366cc0e234b0a399ff99258774d5b
does <ldk980130@gmail.com>
2024-03-25 20:57:58 +0900

    refactor: 동적 조건 구성 메서드 분리

--- src/main/java/com/birca/bircabackend/query/repository/BirthdayCafeDynamicRepositoryImpl.java
+++ src/main/java/com/birca/bircabackend/query/repository/BirthdayCafeDynamicRepositoryImpl.java
@@ -6,6 +6,7 @@ import com.birca.bircabackend.command.like.domain.LikeTargetType;
 import com.birca.bircabackend.query.dto.BirthdayCafeParams;
 import com.birca.bircabackend.query.dto.PagingParams;
 import com.birca.bircabackend.query.repository.model.BirthdayCafeView;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -31,17 +32,11 @@ public class BirthdayCafeDynamicRepositoryImpl implements BirthdayCafeDynamicRep
     public List<BirthdayCafeView> findByBirthdayCafes(Long visitantId,
                                                       BirthdayCafeParams birthdayCafeParams,
                                                       PagingParams pagingParams) {
-        Long cursor = pagingParams.getCursor();
-        LocalDateTime cursorStartDate = findCursorStartDate(cursor);
         return queryFactory.select(Projections.constructor(BirthdayCafeView.class,
                         birthdayCafe, birthdayCafeImage, artist, artistGroup, like))
                 .from(birthdayCafe)
-                .join(artist).on(
-                        birthdayCafe.artistId.eq(artist.id)
-                )
-                .leftJoin(artistGroup).on(
-                        artistGroup.id.eq(artist.groupId)
-                )
+                .join(artist).on(birthdayCafe.artistId.eq(artist.id))
+                .leftJoin(artistGroup).on(artistGroup.id.eq(artist.groupId))
                 .leftJoin(birthdayCafeImage).on(
                         birthdayCafe.id.eq(birthdayCafeImage.birthdayCafeId)
                                 .and(birthdayCafeImage.isMain.isTrue())
@@ -52,17 +47,23 @@ public class BirthdayCafeDynamicRepositoryImpl implements BirthdayCafeDynamicRep
                                 .and(like.visitantId.eq(visitantId))
                 )
                 .where(birthdayCafe.visibility.eq(Visibility.PUBLIC))
-                .where(DynamicBooleanBuilder.builder()
-                        .and(() -> birthdayCafe.progressState.eq(ProgressState.valueOf(birthdayCafeParams.getProgressState())))
-                        .and(() -> birthdayCafe.artistId.eq(birthdayCafeParams.getArtistId()))
-                        .and(() -> birthdayCafe.cafeId.eq(birthdayCafeParams.getCafeId()))
-                        .and(() -> birthdayCafe.schedule.startDate.eq(cursorStartDate).and(birthdayCafe.id.gt(cursor))
-                                .or(birthdayCafe.schedule.startDate.gt(cursorStartDate)))
-                        .build()
-                )
+                .where(generateDynamicCondition(birthdayCafeParams, pagingParams))
                 .orderBy(birthdayCafe.schedule.startDate.asc(), birthdayCafe.id.asc())
                 .limit(pagingParams.getSize())
                 .fetch();
+    }
+
+    private BooleanBuilder generateDynamicCondition(BirthdayCafeParams birthdayCafeParams,
+                                                    PagingParams pagingParams) {
+        Long cursor = pagingParams.getCursor();
+        LocalDateTime cursorStartDate = findCursorStartDate(cursor);
+        return DynamicBooleanBuilder.builder()
+                .and(() -> birthdayCafe.progressState.eq(ProgressState.valueOf(birthdayCafeParams.getProgressState())))
+                .and(() -> birthdayCafe.artistId.eq(birthdayCafeParams.getArtistId()))
+                .and(() -> birthdayCafe.cafeId.eq(birthdayCafeParams.getCafeId()))
+                .and(() -> birthdayCafe.schedule.startDate.eq(cursorStartDate).and(birthdayCafe.id.gt(cursor))
+                        .or(birthdayCafe.schedule.startDate.gt(cursorStartDate)))
+                .build();
     }
 
     private LocalDateTime findCursorStartDate(Long cursor) {
