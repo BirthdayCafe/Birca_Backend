f272864ef87359fddbcb954568d3612de4e36030
CMHyuk <alsgur9710@naver.com>
2024-04-08 18:09:38 +0900

    feat: 자신의 프로필 조회 기능

--- src/main/java/com/birca/bircabackend/query/service/MemberQueryService.java
+++ src/main/java/com/birca/bircabackend/query/service/MemberQueryService.java
@@ -1,11 +1,17 @@
 package com.birca.bircabackend.query.service;
 
+import com.birca.bircabackend.command.auth.authorization.LoginMember;
+import com.birca.bircabackend.command.member.domain.Member;
 import com.birca.bircabackend.command.member.domain.Nickname;
+import com.birca.bircabackend.common.EntityUtil;
 import com.birca.bircabackend.query.dto.NicknameCheckResponse;
+import com.birca.bircabackend.query.dto.ProfileResponse;
 import com.birca.bircabackend.query.repository.MemberQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.birca.bircabackend.command.member.exception.MemberErrorCode.*;
 
 @Service
 @Transactional(readOnly = true)
@@ -13,9 +19,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberQueryService {
 
     private final MemberQueryRepository memberQueryRepository;
+    private final EntityUtil entityUtil;
 
     public NicknameCheckResponse checkNickname(String nickname) {
         Boolean isDuplicated = memberQueryRepository.existsByNickname(new Nickname(nickname));
         return new NicknameCheckResponse(isDuplicated);
+    }
+
+    public ProfileResponse getMyProfile(LoginMember loginMember) {
+        Member member = entityUtil.getEntity(Member.class, loginMember.id(), MEMBER_NOT_FOUND);
+        String nickname = member.getNickname().getValue();
+        return new ProfileResponse(nickname);
     }
 }
