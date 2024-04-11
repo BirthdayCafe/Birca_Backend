bde8514eb636f2d6dd5b596b8186bba6b4c880e9
CMHyuk <alsgur9710@naver.com>
2024-04-11 11:15:53 +0900

    feat: 카페 이미지 업로드 기능

---
+++ src/main/java/com/birca/bircabackend/command/cafe/application/CafeImageFacade.java
@@ -0,0 +1,24 @@
+package com.birca.bircabackend.command.cafe.application;
+
+import com.birca.bircabackend.command.cafe.domain.Cafe;
+import com.birca.bircabackend.command.cafe.exception.CafeImageErrorCode;
+import com.birca.bircabackend.common.EntityUtil;
+import com.birca.bircabackend.common.image.ImageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+@RequiredArgsConstructor
+public class CafeImageFacade {
+
+    private final CafeImageService cafeImageService;
+    private final ImageRepository imageRepository;
+    private final EntityUtil entityUtil;
+
+    public void uploadCafeImage(MultipartFile cafeImage, Long cafeId) {
+        entityUtil.getEntity(Cafe.class, cafeId, CafeImageErrorCode.NOT_FOUND);
+        String imageUrl = imageRepository.upload(cafeImage);
+        cafeImageService.save(cafeId, imageUrl);
+    }
+}
