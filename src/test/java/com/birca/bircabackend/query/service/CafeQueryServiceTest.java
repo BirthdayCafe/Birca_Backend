9c20a85386ac695861ed6b75181287b244ee27c6
CMHyuk <alsgur9710@naver.com>
2024-04-11 22:39:29 +0900

    test: 카페 상세 조회 기능 서비스 테스트

--- src/test/java/com/birca/bircabackend/query/service/CafeQueryServiceTest.java
+++ src/test/java/com/birca/bircabackend/query/service/CafeQueryServiceTest.java
@@ -1,5 +1,9 @@
 package com.birca.bircabackend.query.service;
 
+import com.birca.bircabackend.command.auth.authorization.LoginMember;
+import com.birca.bircabackend.command.cafe.exception.CafeErrorCode;
+import com.birca.bircabackend.common.exception.BusinessException;
+import com.birca.bircabackend.query.dto.CafeDetailResponse;
 import com.birca.bircabackend.query.dto.CafeResponse;
 import com.birca.bircabackend.support.enviroment.ServiceTest;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +15,7 @@ import org.springframework.test.context.jdbc.Sql;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Sql("/fixture/cafe-fixture.sql")
 class CafeQueryServiceTest extends ServiceTest {
@@ -51,4 +56,40 @@ class CafeQueryServiceTest extends ServiceTest {
         }
     }
 
+    @Nested
+    @DisplayName("카페 상세 조회할 때")
+    class FindCafeDetailTest {
+
+        @Test
+        void 정상적으로_조회한다() {
+            // given
+            LoginMember loginMember = new LoginMember(1L);
+
+            // when
+            CafeDetailResponse actual = cafeQueryService.findCafeDetail(loginMember);
+
+            // then
+            assertThat(actual).isEqualTo(new CafeDetailResponse(
+                    "미스티우드",
+                    "경기도 시흥시 은계중앙로 115",
+                    "@ChaseM",
+                    "6시 - 22시",
+                    List.of(new CafeDetailResponse.CafeMenuResponse("아메리카노", 1500)),
+                    List.of(new CafeDetailResponse.CafeOptionResponse("액자", 2000)),
+                    List.of("image1.com", "image2.com", "image3.com", "image4.com", "image5.com")
+            ));
+        }
+
+        @Test
+        void 존재하지_않는_카페는_예외가_발생한다() {
+            // given
+            LoginMember loginMember = new LoginMember(100L);
+
+            // when then
+            assertThatThrownBy(() -> cafeQueryService.findCafeDetail(loginMember))
+                    .isInstanceOf(BusinessException.class)
+                    .extracting("errorCode")
+                    .isEqualTo(CafeErrorCode.NOT_FOUND);
+        }
+    }
 }
--- src/test/resources/fixture/cafe-fixture.sql
+++ src/test/resources/fixture/cafe-fixture.sql
@@ -1,8 +1,24 @@
 SET foreign_key_checks = 0;
 
+INSERT INTO member (id, nickname, email, social_id, social_provider, member_role)
+VALUES (1, '더즈', 'ldk@gmail.com', '231323', 'kakao', 'VISITANT');
+
 INSERT INTO cafe (id, owner_id, business_license_id, name, address, twitter_account, business_hours)
 VALUES (1, 1, 1, '미스티우드', '경기도 시흥시 은계중앙로 115', '@ChaseM', '6시 - 22시'),
        (2, 2, 2, '우지커피', '경기도 성남시 분당구 판교역로 235', '@ChaseM', '8시 - 22시'),
        (3, 3, 3, '메가커피', '서울특별시 강남구 테헤란로 212', '@ChaseM', '9시 - 22시');
+
+INSERT INTO cafe_image (id, cafe_id, image_url)
+VALUES (1, 1, 'image1.com'),
+       (2, 1, 'image2.com'),
+       (3, 1, 'image3.com'),
+       (4, 1, 'image4.com'),
+       (5, 1, 'image5.com');
+
+INSERT INTO cafe_menu(cafe_id, name, price)
+VALUES (1, '아메리카노', 1500);
+
+INSERT INTO cafe_option(cafe_id, name, price)
+VALUES (1, '액자', 2000);
 
 SET foreign_key_checks = 1;
