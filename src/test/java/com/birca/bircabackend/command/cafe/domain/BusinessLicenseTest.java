73beb6b2256f7622a51df93650d3ca3d5886ed15
CMHyuk <alsgur9710@naver.com>
2024-04-18 18:49:00 +0900

    test: 사업자등록증 생성 시 승인 여부 값 테스트

--- src/test/java/com/birca/bircabackend/command/cafe/domain/BusinessLicenseTest.java
+++ src/test/java/com/birca/bircabackend/command/cafe/domain/BusinessLicenseTest.java
@@ -1,28 +1,30 @@
 package com.birca.bircabackend.command.cafe.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BusinessLicenseTest {
 
-    @Test
-    void 사업자등록증을_정상적으로_생성한다() {
-        // when
-        BusinessLicense businessLicense = BusinessLicense.createBusinessLicense(
-                1L, "최민혁",
-                "커피 벌스데이",
-                BusinessLicenseCode.from("123-45-67890"),
-                "서울 마포구 와우산로29길 26-33 1층 커피 벌스데이",
-                "businessLicense.pdf"
-        );
+    @Nested
+    @DisplayName("사업자등록증을 생성할 때")
+    class CreateBusinessLicenseTest {
 
-        // then
-        assertThat(businessLicense.getCafeName()).isEqualTo("커피 벌스데이");
-        assertThat(businessLicense.getOwnerName()).isEqualTo("최민혁");
-        assertThat(businessLicense.getAddress()).isEqualTo("서울 마포구 와우산로29길 26-33 1층 커피 벌스데이");
-        assertThat(businessLicense.getCode().getTaxOfficeCode()).isEqualTo("123");
-        assertThat(businessLicense.getCode().getBusinessTypeCode()).isEqualTo("45");
-        assertThat(businessLicense.getCode().getSerialCode()).isEqualTo("67890");
+        @Test
+        void 승인_여부는_false이다() {
+            // when
+            BusinessLicense businessLicense = BusinessLicense.createBusinessLicense(
+                    1L, "최민혁",
+                    "커피 벌스데이",
+                    BusinessLicenseCode.from("123-45-67890"),
+                    "서울 마포구 와우산로29길 26-33 1층 커피 벌스데이",
+                    "businessLicense.pdf"
+            );
+
+            // then
+            assertThat(businessLicense.getRegistrationApproved()).isFalse();
+        }
     }
 }
