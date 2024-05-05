f142109af4e2a0a457c22447e1fadc533b222a4b
CMHyuk <alsgur9710@naver.com>
2024-05-05 21:32:32 +0900

    feat: 카페 찜하기 취소 기능

--- src/main/java/com/birca/bircabackend/command/like/application/CafeLikeService.java
+++ src/main/java/com/birca/bircabackend/command/like/application/CafeLikeService.java
@@ -5,9 +5,12 @@ import com.birca.bircabackend.command.like.domain.Like;
 import com.birca.bircabackend.command.like.domain.LikeRepository;
 import com.birca.bircabackend.command.like.domain.LikeTarget;
 import com.birca.bircabackend.command.like.domain.LikeTargetType;
+import com.birca.bircabackend.common.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.birca.bircabackend.command.like.exception.LikeErrorCode.INVALID_CANCEL;
 
 @Service
 @Transactional
@@ -21,5 +24,16 @@ public class CafeLikeService {
         LikeTarget likeTarget = new LikeTarget(cafeId, LikeTargetType.CAFE);
         Like like = Like.create(loginMember.id(), likeTarget, likeValidator);
         likeRepository.save(like);
+    }
+
+    public void cancelLike(Long cafeId, LoginMember loginMember) {
+        LikeTarget likeTarget = new LikeTarget(cafeId, LikeTargetType.CAFE);
+        likeRepository.findByVisitantIdAndTarget(loginMember.id(), likeTarget)
+                .ifPresentOrElse(
+                        likeRepository::delete,
+                        () -> {
+                            throw BusinessException.from(INVALID_CANCEL);
+                        }
+                );
     }
 }
--- src/main/java/com/birca/bircabackend/command/like/presentation/CafeLikeController.java
+++ src/main/java/com/birca/bircabackend/command/like/presentation/CafeLikeController.java
@@ -5,10 +5,7 @@ import com.birca.bircabackend.command.auth.authorization.RequiredLogin;
 import com.birca.bircabackend.command.like.application.CafeLikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -21,6 +18,13 @@ public class CafeLikeController {
     @RequiredLogin
     public ResponseEntity<Void> like(@PathVariable Long cafeId, LoginMember loginMember) {
         cafeLikeService.like(cafeId, loginMember);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/v1/cafes/{cafeId}/like")
+    @RequiredLogin
+    public ResponseEntity<Void> cancelLike(@PathVariable Long cafeId, LoginMember loginMember) {
+        cafeLikeService.cancelLike(cafeId, loginMember);
         return ResponseEntity.ok().build();
     }
 }
