e5b4cf52e52452c92af6203338ce679c4c67ed17
CMHyuk <alsgur9710@naver.com>
2024-07-05 13:42:41 +0900

    test: 관리자 인증 추가로 테스트 코드 수정

--- src/test/java/com/birca/bircabackend/admin/service/AdminServiceTest.java
+++ src/test/java/com/birca/bircabackend/admin/service/AdminServiceTest.java
@@ -1,5 +1,6 @@
 package com.birca.bircabackend.admin.service;
 
+import com.birca.bircabackend.admin.dto.AdminAuthRequest;
 import com.birca.bircabackend.command.cafe.domain.Cafe;
 import com.birca.bircabackend.common.exception.BusinessException;
 import com.birca.bircabackend.support.enviroment.ServiceTest;
@@ -29,13 +30,15 @@ class AdminServiceTest extends ServiceTest {
     @DisplayName("사업자등록증 승인 시")
     class ApproveBusinessLicenseTest {
 
+        private static final AdminAuthRequest REQUEST = new AdminAuthRequest("id", "password");
+
         @Test
         void 정상적으로_승인한다() {
             // given
             Long businessLicenseId = 1L;
 
             // when
-            adminService.approveBusinessLicense(businessLicenseId);
+            adminService.approveBusinessLicense(businessLicenseId, REQUEST);
             Cafe expectedCafe = entityManager.createQuery("select c from Cafe c where c.businessLicenseId = :businessLicenseId", Cafe.class)
                     .setParameter("businessLicenseId", businessLicenseId)
                     .getSingleResult();
@@ -54,7 +57,19 @@ class AdminServiceTest extends ServiceTest {
 
                     () -> assertThat(expectedRegistrationApproved).isTrue()
             );
+        }
 
+        @Test
+        void 관리자_아이디_비밀번호가_일치하지_않으면_예외가_발생한다() {
+            // given
+            Long businessLicenseId = 1L;
+            AdminAuthRequest request = new AdminAuthRequest("wrong-id", "wrong-password");
+
+            // when then
+            assertThatThrownBy(() -> adminService.approveBusinessLicense(businessLicenseId, request))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .extracting("message")
+                    .isEqualTo("잘못된 관리자 인증 정보입니다.");
         }
 
         @Test
@@ -63,7 +78,7 @@ class AdminServiceTest extends ServiceTest {
             Long businessLicenseId = 100L;
 
             // when then
-            assertThatThrownBy(() -> adminService.approveBusinessLicense(businessLicenseId))
+            assertThatThrownBy(() -> adminService.approveBusinessLicense(businessLicenseId, REQUEST))
                     .isInstanceOf(BusinessException.class)
                     .extracting("errorCode")
                     .isEqualTo(BUSINESS_LICENSE_NOT_FOUND);
