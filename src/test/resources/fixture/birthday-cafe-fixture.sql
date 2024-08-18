d4e7f44199c3b8a91894853016e79466a607afa1
CMHyuk <alsgur9710@naver.com>
2024-08-18 16:16:29 +0900

    test: 탈퇴 회원 카페 테스트 데이터 추가

--- src/test/resources/fixture/birthday-cafe-fixture.sql
+++ src/test/resources/fixture/birthday-cafe-fixture.sql
@@ -6,7 +6,8 @@ VALUES (1, '더즈', 'ldk@gmail.com', '231323', 'kakao', 'HOST'), -- 주최자1
        (2, '민혁', 'cm@gmail.com', '341234', 'kakao', 'HOST'), -- 주최자2
        (3, '카페1 사장', 'owner@gmail.com', '23412', 'kakao', 'OWNER'), -- 사장님
 
-       (4, '방문자', 'visitant@gmail.com', '232', 'kakao', 'VISITANT'); -- 방문자
+       (4, '방문자', 'visitant@gmail.com', '232', 'kakao', 'VISITANT'), -- 방문자
+       (5, '카페2 사장', 'owner2@gmail.com', '23422', 'kakao', 'DELETED');
 
 -- 사장님의 사업자 등록증
 INSERT INTO business_license(id, owner_id, owner_name, cafe_name, tax_office_code, business_type_code, serial_code,
@@ -47,6 +48,8 @@ VALUES (1, null, 1, 1, 1, 3, '2024-02-08T00:00:00', '2024-02-10T00:00:00', 5, 10
        (5, '윈터의 생일 카페', 2, 3, 1, 3, '2024-02-11T00:00:00', '2024-02-12T00:00:00', 5, 10, '@ChaseM', '010-0000-0000',
         'RENTAL_APPROVED','PUBLIC', 'SMOOTH', 'ABUNDANT'), -- 주최자1의 대관 승인 카페
        (6, null, 2, 1, 2, 3, '2024-02-11T00:00:00', '2024-02-14T00:00:00', 5, 10, '@ChaseM', '010-0000-0000',
+        'IN_PROGRESS','PUBLIC', 'SMOOTH', 'ABUNDANT'),
+       (7, '윈터의 생일 카페', 2, 1, 2, 5, '2024-02-11T00:00:00', '2024-02-14T00:00:00', 5, 10, '@ChaseM', '010-0000-0000',
         'IN_PROGRESS','PUBLIC', 'SMOOTH', 'ABUNDANT');
 
 
